8c3f7872f365beb5096f8c3461955f30715fa731
Elizaveta-Kazakova <e.kazakova3@g.nsu.ru>
2022-09-20 20:44:49 +0700

    improve error handling

--- lab_4/lab_4.c
+++ lab_4/lab_4.c
@@ -11,14 +11,15 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
-#include <ctype.h>
 
 #define NUM_OF_STEPS 200000000
 #define INDEX_FOR_NUM_OF_THREADS 1
 #define BUF_SIZE 1024
 #define SUCCESS_CODE 0
 #define MAX_INVALID_ARG 0
-#define MESSAGE_FOR_INVALID_ARG "Please write number greater than 0"
+#define MESSAGE_FOR_INVALID_ARG "Please write number greater than 0\n"
+#define NUM_OF_ARGS 2
+#define MESSAGE_FOR_INVALID_ARGS_NUM "Please write one argument\n"
 
 typedef struct partial_sum_args {
     int start_index;
@@ -41,6 +42,17 @@ int convert_index_to_number(int index) {
     return index + 1;
 }
 
+int join_threads_with_partial_sum(int num_of_threads, pthread_t *threads_id, partial_sum_args *threads_args, double *sum) {
+    for (int thread_num = 0; thread_num < num_of_threads; ++thread_num) {
+        int return_code = pthread_join(threads_id[thread_num], NULL);
+        if (return_code != SUCCESS_CODE) {
+    	    return return_code;
+	}
+        *sum += threads_args[thread_num].result;
+    }
+    return SUCCESS_CODE;
+}
+
 int create_threads_for_partial_sum(int num_of_threads, pthread_t *threads_id, partial_sum_args *threads_args) {
     int iteration_num_for_thread = NUM_OF_STEPS / num_of_threads;
     int additional_iterations = NUM_OF_STEPS % num_of_threads;
@@ -52,48 +64,53 @@ int create_threads_for_partial_sum(int num_of_threads, pthread_t *threads_id, pa
 	}
         int return_code = pthread_create(&threads_id[thread_num], 
                                         NULL, &calc_partial_sum, (void *)&threads_args[thread_num]); 
-        if (return_code != SUCCESS_CODE) {
+	if (return_code != SUCCESS_CODE) {
             return return_code; 
         }
     }
     return SUCCESS_CODE;
 }
 
-void print_error(int return_code) {
+void print_error(int return_code, char *additional_message) {
     char buf[BUF_SIZE];
     strerror_r(return_code, buf, sizeof buf);
-    fprintf(stderr, "creating thread : %s\n", buf);
+    fprintf(stderr, "%s: %s\n", additional_message, buf);
 }
 
 int main(int argc, char **argv) {
     // read arg and check for validity
+    if (argc != NUM_OF_ARGS) {
+	printf(MESSAGE_FOR_INVALID_ARGS_NUM);
+	exit(EXIT_SUCCESS);
+    }
     int num_of_threads = atoi(argv[INDEX_FOR_NUM_OF_THREADS]);
     if (num_of_threads <= MAX_INVALID_ARG) {
 	printf(MESSAGE_FOR_INVALID_ARG);
 	exit(EXIT_SUCCESS);
     }
-   
+
     // allocation of memory
     pthread_t *threads_id = (pthread_t *)malloc(num_of_threads * sizeof(pthread_t));
     partial_sum_args *threads_args = (partial_sum_args *)malloc(num_of_threads * sizeof(partial_sum_args));
+    if (threads_id == NULL || threads_args == NULL) {
+	perror("malloc error ");
+	exit(EXIT_FAILURE);
+    } 	
 
     // create threads and run functions
     int return_code = create_threads_for_partial_sum(num_of_threads, threads_id, threads_args); 
     if (return_code != SUCCESS_CODE) {
-	print_error(return_code);
+	print_error(return_code, "creating thread");
         exit(EXIT_FAILURE); 
     }
 
     double pi = 0;
    
     // join threads and sum partial sums
-    for (int thread_num = 0; thread_num < num_of_threads; ++thread_num) {
-	int return_code = pthread_join(threads_id[thread_num], NULL);
-	if (return_code != SUCCESS_CODE) {
-            print_error(return_code);
-            exit(EXIT_FAILURE); 
-        }
-	pi += threads_args[thread_num].result;
+    return_code = join_threads_with_partial_sum(num_of_threads, threads_id, threads_args, &pi);
+    if (return_code != SUCCESS_CODE) {
+	print_error(return_code, "join thread");
+        exit(EXIT_FAILURE); 
     }
 
     // free memory
