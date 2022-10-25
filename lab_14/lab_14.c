ea5ad22ac2919728f347bf2ad2248c0aa1f9a627
Elizaveta-Kazakova <e.kazakova3@g.nsu.ru>
2022-10-25 21:04:09 +0700

    lab_14: add handling error

--- lab_14/lab_14.c
+++ lab_14/lab_14.c
@@ -18,36 +18,55 @@
 #define SEMAPHORE_INDEX_FOR_PARENT 1
 #define INITIAL_VALUE_OF_FISRT_SEM 0
 #define INITIAL_VALUE_OF_SECOND_SEM 1
-
+#define DO_NOT_TO_EXECUTE_FUNCTION 0
 
 struct print_args {
     char *message;
     int num_of_str;
     int index_of_semaphore_to_wait;
+    pthread_t another_thread_id;
+};
+
+struct destroy_args {
+    sem_t *semaphores;
+    int num_of_semaphores;
+    int destroy_res;
 };
 
 sem_t semaphores[NUM_OF_SEMAPHORES];
 
-int destroy_semaphores(sem_t *semaphores, int num_of_semaphores) {
-    int destroy_res = SUCCESS_CODE;
-    for (int sem_num = 0; sem_num < num_of_semaphores; ++sem_num) {  
-	int return_code = sem_destroy(&semaphores[sem_num]);
-	if (return_code != SUCCESS_CODE && destroy_res == SUCCESS_CODE) {
-	    destroy_res = return_code;
+void destroy_semaphores(void *arg) {
+    int return_code;
+    struct destroy_args *args = (struct destroy_args *)arg;
+    args->destroy_res = SUCCESS_CODE;
+    for (int sem_num = 0; sem_num < args->num_of_semaphores; ++sem_num) {  
+	int return_code = sem_destroy(&args->semaphores[sem_num]);
+	if (return_code != SUCCESS_CODE && args->destroy_res == SUCCESS_CODE) {
+	    args->destroy_res = return_code;
 	}
     }
-    return destroy_res;
 }
 
 void *print_n_str(void *arg) {
+    int return_code;
     struct print_args *args = (struct print_args *)arg;
     int str_number = 0;
     int index_of_semaphore_to_wait = args->index_of_semaphore_to_wait;
     int index_of_semaphore_to_post = (index_of_semaphore_to_wait + 1) % (NUM_OF_SEMAPHORES);
     while (str_number < args->num_of_str) {
-        sem_wait(&semaphores[index_of_semaphore_to_wait]);
+        return_code = sem_wait(&semaphores[index_of_semaphore_to_wait]);
+	if (return_code != SUCCESS_CODE) {
+	    perror("waiting for a semaphore");
+	    pthread_cancel(args->another_thread_id);
+	    break;
+	}
 	printf("%s with %d number of string\n", args->message, str_number);
-	sem_post(&semaphores[index_of_semaphore_to_post]);
+	return_code = sem_post(&semaphores[index_of_semaphore_to_post]);
+	if (return_code != SUCCESS_CODE) {
+            perror("posting semaphore");
+            pthread_cancel(args->another_thread_id);
+	    break;
+        }
         ++str_number;
     }
     return NULL;
@@ -59,7 +78,8 @@ int init_semaphores(sem_t *semaphores, int num_of_semaphores, unsigned int *init
 	return_code = sem_init(&semaphores[sem_num], SHARED_BETWEEN_THREADS_OF_A_PROCCESS, 
 							initial_value_of_semaphores[sem_num]);
 	if (return_code != SUCCESS_CODE) {
-	    destroy_semaphores(semaphores, sem_num);
+	    struct destroy_args args_for_destroy= {semaphores, sem_num};
+	    destroy_semaphores((void *)&args_for_destroy);
 	    return return_code;
 	}
     }
@@ -73,8 +93,12 @@ void print_error(int return_code, char *additional_message) {
 }
 
 int main() {
+    struct destroy_args args_for_destroy_all_sems = {semaphores, NUM_OF_SEMAPHORES};
+    pthread_cleanup_push(destroy_semaphores, (void *)&args_for_destroy_all_sems);
     int return_code;
-    unsigned int initial_value_of_semaphores[] = {INITIAL_VALUE_OF_FISRT_SEM, INITIAL_VALUE_OF_SECOND_SEM};
+    pthread_t main_thread_id = pthread_self();
+    unsigned int initial_value_of_semaphores[] = {INITIAL_VALUE_OF_FISRT_SEM, INITIAL_VALUE_OF_SECOND_SEM, 
+							main_thread_id};
     return_code = init_semaphores(semaphores, NUM_OF_SEMAPHORES, initial_value_of_semaphores);
     if (return_code != SUCCESS_CODE) {
 	perror("initializing semaphores");
@@ -84,23 +108,24 @@ int main() {
     struct print_args args_for_child = {CHILD_MESSAGE, NUM_OF_STR, SEMAPHORE_INDEX_FOR_CHILD};
     return_code = pthread_create(&thread_id, NULL, print_n_str, (void *)&args_for_child);
     if (return_code != SUCCESS_CODE) {
-	destroy_semaphores(semaphores, NUM_OF_SEMAPHORES);
+	destroy_semaphores((void *)&args_for_destroy_all_sems);
         print_error(return_code, "creating thread");
         exit(EXIT_FAILURE);
     }
-    struct print_args args_for_parent = {PARENT_MESSAGE, NUM_OF_STR, SEMAPHORE_INDEX_FOR_PARENT};
+    struct print_args args_for_parent = {PARENT_MESSAGE, NUM_OF_STR, SEMAPHORE_INDEX_FOR_PARENT, thread_id};
     print_n_str((void *)&args_for_parent);
     return_code = pthread_join(thread_id, NULL);
     if (return_code != SUCCESS_CODE) {
-	destroy_semaphores(semaphores, NUM_OF_SEMAPHORES);
+	destroy_semaphores((void *)&args_for_destroy_all_sems);
         print_error(return_code, "joining thread");
         exit(EXIT_FAILURE);
     }
-    return_code = destroy_semaphores(semaphores, NUM_OF_SEMAPHORES);
-    if (return_code != SUCCESS_CODE) {
+    destroy_semaphores((void *)&args_for_destroy_all_sems);
+    if (args_for_destroy_all_sems.destroy_res != SUCCESS_CODE) {
 	perror("destroy semaphores");
 	exit(EXIT_FAILURE);
     }
+    pthread_cleanup_pop(DO_NOT_TO_EXECUTE_FUNCTION);
     exit(EXIT_SUCCESS);
 }
 
