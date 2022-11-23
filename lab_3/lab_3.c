854eeaa45d1ccd63c1b2000d98b62c0889cbed10
Elizaveta-Kazakova <e.kazakova3@g.nsu.ru>
2022-11-23 13:03:39 +0700

    lab_3: fix join threads

--- lab_3/lab_3.c
+++ lab_3/lab_3.c
@@ -21,6 +21,17 @@ void *print_message(void *arg) {
     return NULL;
 }
 
+int join_threads(pthread_t *thread_ids, int num_of_threads) {
+    int return_code = SUCCESS_CODE;
+    for (int thread_num = 0; thread_num < num_of_threads; ++thread_num) {
+	int join_code =  pthread_join(thread_ids[thread_num], NULL);
+        if (join_code != SUCCESS_CODE && return_code == SUCCESS_CODE) {
+	    return_code = join_code;
+        }
+    }
+    return return_code;
+}
+
 void print_error(int return_code, char *additional_message) {
     char buf[BUF_SIZE];
     strerror_r(return_code, buf, sizeof buf);
@@ -29,7 +40,7 @@ void print_error(int return_code, char *additional_message) {
 
 int main() {
     int return_code;
-    pthread_t threads_id[NUM_OF_THREADS];
+    pthread_t thread_ids[NUM_OF_THREADS];
     char *messages[NUM_OF_STR][NUM_OF_THREADS] = {
 	{"Thread number 1, line 1\n", "Thread number 1, line 2\n", "Thread number 1, line 3\n", "Thread number 1, line 4\n"},
 	{"Thread number 2, line 1\n", "Thread number 2, line 2\n", "Thread number 2, line 3\n", "Thread number 2, line 4\n"},
@@ -40,18 +51,17 @@ int main() {
     for (int thread_num = 0; thread_num < NUM_OF_THREADS; ++thread_num) {
 	message_args[thread_num].message = messages[thread_num];
 	message_args[thread_num].num_of_strings = NUM_OF_STR;
-	return_code = pthread_create(&threads_id[thread_num], NULL, print_message, (void *)&message_args[thread_num]);
+	return_code = pthread_create(&thread_ids[thread_num], NULL, print_message, (void *)&message_args[thread_num]);
     	if (return_code != SUCCESS_CODE) {
             print_error(return_code, "creating thread");
+	    join_threads(thread_ids, thread_num);
 	    exit(EXIT_FAILURE);
 	}
     }
-    for (int thread_num = 0; thread_num < NUM_OF_THREADS; ++thread_num) {
-	return_code =  pthread_join(threads_id[thread_num], NULL);
-	if (return_code != SUCCESS_CODE) {
-            print_error(return_code, "joining thread");
-	    exit(EXIT_FAILURE);
-	}
+    return_code = join_threads(thread_ids, NUM_OF_THREADS);
+    if (return_code != SUCCESS_CODE) {
+	print_error(return_code, "joining threads");
+	exit(EXIT_FAILURE);
     }
     exit(EXIT_SUCCESS);
 }
